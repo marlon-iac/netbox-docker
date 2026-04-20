96bc6049bff4d30150dc9689b171955c006c55c2
Marlon <msaviol@gmail.com>
2026-04-20 09:11:32 -0300

    removido netbox docker

---
+++ netbox-custom/netbox-slurpit/configuration/plugins.py
@@ -0,0 +1,15 @@
+# Add your plugins and plugin settings here.
+# Of course uncomment this file out.
+
+# To learn how to build images with your required plugins
+# See https://github.com/netbox-community/netbox-docker/wiki/Using-Netbox-Plugins
+
+PLUGINS = [
+"slurpit_netbox",
+]
+
+# PLUGINS_CONFIG = {
+#   "netbox_bgp": {
+#     ADD YOUR SETTINGS HERE
+#   }
+# }
